e5d58c204d12c4d03f6672059e1b88c24a45dcbd
Daniel Dvořák <dv.daniel@centrum.cz>
2015-07-26 18:12:34 +0200

    struktura objektu

--- allocation3.py
+++ allocation3.py
@@ -1,4 +1,5 @@
 
+
 import numpy as np
 from scipy.stats import scoreatpercentile
 from statsmodels.distributions.empirical_distribution import ECDF
@@ -42,56 +43,8 @@ class Losses:
             self.w = w
 
 
-########################
-##### alokacni pravidla:
-########################
-
-"""
-alokacni pravidlo ma metodu allocate a jedine, co dela, je, ze vraci alokace
-"""
-
-
-class AllocRule:
-    def __init__(self, data):
-        """
-        K is the capital to be distributed,
-        losses is a numpy array - a column for each line,
-        xi - value of a random variable for each observation,
-        w - weights
-        """
-        self.data = data
-
-    def _allocate(self):
-        pass
-
-
-class QuantileRule(AllocRule):
-    def quantile(self, p, alpha):
-        sorted_losses = self.data.sorted_losses
-        lower = scoreatpercentile(sorted_losses, p, axis=0, interpolation_method='lower')
-        higher = scoreatpercentile(sorted_losses, p, axis=0, interpolation_method='higher')
-        return alpha*lower + (1-alpha)*higher
-
-
 class QuadraticRule(AllocRule):
     def allocate(self):
         means = self.data.losses.T.dot(self.data.xi)/self.data._n
-        return means + self.data.w*(self.data.K-means.sum())
-
-
-"""
-hlavni trida:
-"""
-
-class Allocations():
-    def __init__(self, *args, **kwargs):
-        self.data = Losses(*args, **kwargs)
-        self.allocations = dict()
-
-    rules = {'quadratic': QuadraticRule}
-
-    def allocate(self, type='quadratic'):
-        self.allocations[type] = self.rules[type](self.data).allocate()
-        print(self.allocations[type])
-        return self
-
+        self.allocations = means + self.data.w*(self.data.K-means.sum())
+        return self.allocations
